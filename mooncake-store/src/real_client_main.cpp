15978697a68c9d63719e9c260858b495e9599c4f
JimmyPeilinLi <lipeilin@mail.nwpu.edu.cn>
2026-04-16 13:06:05 +0800

    rpc server listening to 0.0.0.0

--- mooncake-store/src/real_client_main.cpp
+++ mooncake-store/src/real_client_main.cpp
@@ -113,7 +113,7 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    coro_rpc::coro_rpc_server server(FLAGS_threads, FLAGS_port, "127.0.0.1");
+    coro_rpc::coro_rpc_server server(FLAGS_threads, FLAGS_port, FLAGS_host);
     RegisterClientRpcService(server, *client_inst);
 
     LOG(INFO) << "Starting real client service on 127.0.0.1:" << FLAGS_port;
